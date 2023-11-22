e3c106d3a9820202bbc58542eb23cc63808e997a
Carlos Alberto Soares <carlos.soares.3000@gmail.com>
2023-11-21 21:56:55 -0300

    Validação de CPF

--- 04-formulario/script/valida-cpf.js
+++ 04-formulario/script/valida-cpf.js
@@ -1,22 +1,22 @@
 document.getElementById("validarButton").
     addEventListener("click", function () {
-        //alert("Deu certo!!!");
-
+        // alert("Deu Certo!!!");
         const cpf = document.getElementById("cpfInput").value;
 
         if (validaCPF(cpf)) {
-            document.getElementById("resultado").textContent = "CPF Valido"
+            document.getElementById("resultado").textContent = "CPF Válido"
         } else {
             document.getElementById("resultado").textContent = "CPF Inválido"
         }
+
     })
 
 function validaCPF(vrCPF) {
-    // Implementar as regras de validação do CPF
-    
-    //Implementar as regras de validação do cpf
-    let soma = 0;
 
+    //return vrCPF.length === 11 & !isNaN(vrCPF);
+
+    //Implementar as regras de validação do CPF
+    let soma = 0;
     soma += parseInt(vrCPF.charAt(0)) * 10;
     soma += parseInt(vrCPF.charAt(1)) * 9;
     soma += parseInt(vrCPF.charAt(2)) * 8;
@@ -25,24 +25,41 @@ function validaCPF(vrCPF) {
     soma += parseInt(vrCPF.charAt(5)) * 5;
     soma += parseInt(vrCPF.charAt(6)) * 4;
     soma += parseInt(vrCPF.charAt(7)) * 3;
-   soma += parseInt(vrCPF.charAt(8)) * 2;
+    soma += parseInt(vrCPF.charAt(8)) * 2;
 
-
-    letdigitoX = soma % 11;
+    let digitoX = soma % 11;
     digitoX = 11 - digitoX;
-
-    if (digitoX > 10) {
+    if (digitoX >= 10) {
         digitoX = 0;
-
     }
 
     let digitoValido = true;
     if (digitoX !== parseInt(vrCPF.charAt(9))) {
         digitoValido = false;
-
-        return digitoValido;
-        //return vrCPF.length === 11 & !isNaN(vrCPF);
     }
+    soma = 0;
+    soma += parseInt(vrCPF.charAt(0)) * 11;
+    soma += parseInt(vrCPF.charAt(1)) * 10;
+    soma += parseInt(vrCPF.charAt(2)) * 9;
+    soma += parseInt(vrCPF.charAt(3)) * 8;
+    soma += parseInt(vrCPF.charAt(4)) * 7;
+    soma += parseInt(vrCPF.charAt(5)) * 6;
+    soma += parseInt(vrCPF.charAt(6)) * 5;
+    soma += parseInt(vrCPF.charAt(7)) * 4;
+    soma += parseInt(vrCPF.charAt(8)) * 3;
+    soma += parseInt(vrCPF.charAt(9)) * 2;
+
+    let digitoY = soma % 11;
+    digitoY = 11 - digitoY;
+    if (digitoY >= 10) {
+        digitoY = 0;
+    }
+
+
+    if (digitoY !== parseInt(vrCPF.charAt(10))) {
+        digitoValido = false;
+    }
+
+    return digitoValido;
+    // return vrCPF.length === 11 & !isNaN(vrCPF);
 }
-
-
